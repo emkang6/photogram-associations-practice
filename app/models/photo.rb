d24d88918a4d74399c61a3dddd4e8f2f67cae9a1
emkang6 <hkang6@uchicago.edu>
2024-11-19 05:47:55 +0000

    except scope

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -33,4 +33,6 @@ class Photo < ApplicationRecord
   ## Indirect associations
 
   # Photo#fans: returns rows from the users table associated to this photo through its likes
+  has_many(:fans, :through => :likes, :source => :fan )
+
 end
